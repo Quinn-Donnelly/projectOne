fa56f5bdc3001171dcec2ab7896015d345e82465
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-07-28 14:02:26 -0400

    Employee BEAN made created folder stucture for front controller

---
+++ reimbursementapi/src/main/java/com/revature/api/beans/Employee.java
@@ -0,0 +1,127 @@
+package com.revature.api.beans;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = -368420383273189348L;
+	private int employeeID;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private transient String password;
+	private int managerID;
+	private int address_id;
+	
+	
+	public Employee() {
+		super();
+	}
+	
+	public Employee(int employeeID, String firstName, String lastName, String email, String password, int managerID,
+			int address_id) {
+		super();
+		this.employeeID = employeeID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.managerID = managerID;
+		this.address_id = address_id;
+	}
+	
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getManagerID() {
+		return managerID;
+	}
+	public void setManagerID(int managerID) {
+		this.managerID = managerID;
+	}
+	public int getAddress_id() {
+		return address_id;
+	}
+	public void setAddress_id(int address_id) {
+		this.address_id = address_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeID=" + employeeID + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", managerID=" + managerID + ", address_id=" + address_id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + address_id;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + employeeID;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + managerID;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (address_id != other.address_id)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (employeeID != other.employeeID)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (managerID != other.managerID)
+			return false;
+		return true;
+	}
+}
